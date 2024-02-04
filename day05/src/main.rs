9c51467fbf64ce627f0edc758c86b36095d3bbad
Seyfullah Gündoğdu <seyfullahgundogdu74@gmail.com>
2024-02-05 01:01:24 +0300

    Day 5 part 2 finally done.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, time::Instant};
 
 fn main() {
     // Read the contents of the file into a string
@@ -17,11 +17,18 @@ fn main() {
         .split_whitespace()
         .map(|s| s.parse::<u64>().unwrap())
         .collect();
-
+    let mut maps = vec![vec![]; 7];
+    for (i, chunk) in chunks.iter().enumerate() {
+        chunk.trim().split('\n').skip(1).for_each(|triplet| {
+            let triplet: Vec<u64> = triplet.split(' ').map(|num| num.parse().unwrap()).collect();
+            maps[i - 1].push((triplet[0], triplet[1], triplet[2]));
+        });
+    }
     // Part 1
     // Iterate through the remaining chunks to apply mappings and update the seed values
-    let p1_ans = parse_chunks_part1(&chunks, &initial_seeds);
-    println!("{}", p1_ans);
+    let p1_ans = part1_solution(&maps, &initial_seeds);
+    println!("Part 1 answer: {}", p1_ans);
+
     // Part 2
     // now our seeds are pair of starts and ranges,
     // rewrite initial_seeds array accordingly
@@ -33,40 +40,81 @@ fn main() {
     for i in (0..seed_ranges.len()).step_by(2) {
         initial_seeds.push((seed_ranges[i], seed_ranges[i + 1]));
     }
+    let b = Instant::now();
+    let p2_ans = part2_solution(&maps, &initial_seeds);
+    println!("Part 2 answer: {:?}",b.elapsed());
+    println!("Part 2 answer: {}",p2_ans);
 }
 
-fn parse_chunks_part1(chunks: &[&str], initial_seeds: &Vec<u64>) -> u64 {
-    let mut initial_seeds = initial_seeds.to_owned();
-    for chunk in &chunks[1..] {
-        // Create a copy of the original seeds for each chunk
-        let mut mapped_seeds = initial_seeds.clone();
+fn part1_solution(maps: &[Vec<(u64, u64, u64)>], initial_seeds: &[u64]) -> u64 {
+    let mut seeds = initial_seeds.to_owned();
 
-        // Iterate through non-empty lines in the chunk starting from the second line
-        for line in chunk.lines().filter(|l| !l.is_empty()).skip(1) {
-            // Parse triplets of values from the line in the format [destination, source, range]
-            let map: Vec<u64> = line
-                .split_whitespace()
-                .map(|m| m.parse::<u64>().unwrap())
-                .collect();
-            let (dest, src, range) = (map[0], map[1], map[2]);
+    for map in maps.iter() {
+        let mut mapped_seeds = seeds.clone();
 
-            // Update mapped seeds based on the specified range and mapping
-            for (i, &seed) in initial_seeds.iter().enumerate() {
+        for &(dest, src, range) in map.iter().skip(1) {
+            for (i, &seed) in seeds.iter().enumerate() {
                 if seed >= src && seed < src + range {
-                    // Calculate the new mapped value based on the distance from the source
                     mapped_seeds[i] = dest + (seed - src);
                 }
             }
         }
-        // Update the original seeds with the values from the mapped seeds
-        initial_seeds = mapped_seeds;
+        seeds = mapped_seeds;
     }
 
-    // Return the final result, which is the minimum value in the updated seed list
-    *initial_seeds.iter().min().unwrap()
+    *seeds.iter().min().unwrap()
 }
 
-fn parse_chunks_part2(chunks: &[&str], initial_seeds: &Vec<(u64, u64)>) -> u64 {
-    // let t = RangeInclusive::new(start, end)
-    todo!()
+
+// need fixing, very ugly rust code
+// crabs are gonna hunt me down 
+fn part2_solution(maps: &[Vec<(u64, u64, u64)>], initial_seeds: &[(u64, u64)]) -> u64 {
+    let mut seeds = initial_seeds.to_owned();
+    let mut mapped_results: Vec<(u64, u64)> = vec![];
+    for map in maps {
+        while !seeds.is_empty() {
+            let mut in_range = false;
+            let start = seeds[0].0;
+            let offset = seeds[0].1;
+            for mapping in map {
+                let end = mapping.1 + mapping.2;
+                if start < end  && start >= mapping.1 {
+                    in_range = true;
+                    // source range is entirely covered in range map
+                    if start + offset < end {
+                        mapped_results.push((start + mapping.0 - mapping.1, offset));
+                        let _ = seeds.remove(0);
+                        break;
+                    }
+                    else {
+                        let new_offset = end - start;
+                        mapped_results.push((start + mapping.0 - mapping.1,new_offset));
+                        seeds.push((end, start + offset - end));
+                        seeds.remove(0);
+                        break;
+                    }
+                } 
+                // source's start is less then range's beginning, end is inside range.
+                else if start + offset < mapping.1 + mapping.2 && start + offset > mapping.1 {
+                    in_range = true;
+                    if start >= mapping.1 {
+                        mapped_results.push((start + mapping.0 - mapping.1, offset));
+                        seeds.remove(0);
+                        break;
+                    } else {
+                        mapped_results.push((mapping.0, start + offset - mapping.1));
+                        seeds.push((start, mapping.1 - start));
+                        seeds.remove(0);
+                        break;
+                    }
+                }
+            }
+            if !in_range {
+                mapped_results.push((start, offset));
+                seeds.remove(0);
+            }
+        }
+        std::mem::swap(&mut seeds, &mut mapped_results);
+    }
+    seeds.iter().min_by(|&&a, &&b| a.0.cmp(&b.0)).unwrap().0
 }
